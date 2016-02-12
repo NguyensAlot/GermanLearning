b1b43df2fc297a7a5f5a203bc32616d15a4b2faa
Anthony Nguyen <anthony2014.2@gmail.com>
2016-02-12 09:40:07 +0000

    compartmentalized create flashcard set file and added changed view into erb

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,6 +1,3 @@
 class Flashcard < ActiveRecord::Base
-  validates_presence_of :set_name, :word, :definition
-  validates_length_of :set_name, :minimum > 0
-  validates_length_of :word, :minimum > 0
-  validates_length_of :definition, :minimum > 0
+
 end
