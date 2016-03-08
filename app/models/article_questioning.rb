80cc4124358176080fe6cbfe13a7abfa45699e6e
Anthony Nguyen <anthony2014.2@gmail.com>
2016-03-08 02:16:13 +0000

    modified article questioning table attributes

--- app/models/article_questioning.rb
+++ app/models/article_questioning.rb
@@ -1,5 +1,5 @@
 class ArticleQuestioning < ActiveRecord::Base
-  attr_accessible :quiz_results_attributes
+  attr_accessible :quiz_results_attributes, :article, :questions, :answer_choices, :correct_answers, :num_of_questions, :num_of_answer_choices
   has_many :quiz_results
   accepts_nested_attributes_for :quiz_results  
 end
