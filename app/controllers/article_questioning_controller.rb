a6f0f9bd0d865d5899e3d1f2d0b4f0afcedac970
Anthony Nguyen <anthony2014.2@gmail.com>
2016-04-17 02:28:38 +0000

    cleaned up essay sim and updated validations

--- app/controllers/article_questioning_controller.rb
+++ app/controllers/article_questioning_controller.rb
@@ -14,23 +14,3 @@ class ArticleQuestioningController < ApplicationController
     end
   end
 end
-=begin
-20160225204357_create_article_questionings.rb
-
-class CreateArticleQuestionings < ActiveRecord::Migration
-  def change
-    create_table :article_questionings do |t|
-      t.text :article
-      t.text :questions
-      t.text :answer_choices
-      t.text :correct_answers
-      t.integer :num_of_questions
-      t.integer :num_of_answer_choices
-      
-
-      t.timestamps null: false
-    end
-  end
-end
-
-=end
